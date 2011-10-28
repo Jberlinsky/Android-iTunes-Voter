564dcd8c31f10459f0fa48bf9760eb34117e1d2b
Jack Wink <jackwink@umich.edu>
2011-10-28 11:53:15 -0400

    Added initial android app

---
+++ app/src/com/jackandjason/voter.java
@@ -0,0 +1,13 @@
+package com.jackandjason;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class voter extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

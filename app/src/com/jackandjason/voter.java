4ee3fb7dc94bba5adff681ce2a9d3e3bfbc9e475
Jason Berlinsky <jberlinsky@gmail.com>
2011-10-30 17:31:01 -0400

    Adds error detection to Android client; if host is not available, cry a little.

--- app/src/com/jackandjason/voter.java
+++ app/src/com/jackandjason/voter.java
@@ -11,29 +11,78 @@ import android.widget.TextView;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.KeyEvent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import java.net.InetAddress;
+import android.content.Intent;
 
 public class voter extends Activity {
     /** Called when the activity is first created. */
 
 	WebView webview;
+	private String HOST = "141.212.236.105";
+	private String HOST_ADDRESS = "http://" + HOST + ":8888/vote";
 	private class VoteViewClient extends WebViewClient {
+		public Activity context;
 		@Override
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
 			view.loadUrl(url);
 			return true;
 		}
-	}    
+
+		@Override
+			public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+			{
+		  	this.showError();
+			}
+	private void showError()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this.context);
+		  	builder.setCancelable(true);
+		  	//builder.setIcon(R.drawable.dialog_question);
+		  	builder.setTitle("Host not Reachable: " + HOST_ADDRESS);
+		  	builder.setInverseBackgroundForced(true);
+		  	builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+		  		@Override
+		  		public void onClick(DialogInterface dialog, int which)
+					{
+						// Retru connection
+						Intent intent = getIntent();
+						finish();
+						startActivity(intent);
+						dialog.dismiss();
+					}
+				});
+			builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// Close the application
+		  		dialog.dismiss();
+		  		Intent intent = new Intent(Intent.ACTION_MAIN);
+		  		intent.addCategory(Intent.CATEGORY_HOME);
+		  		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		  		startActivity(intent);
+				}
+			});
+			AlertDialog alert = builder.create();
+			alert.show();
+	}
+	}
+
 	@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			webview = new WebView(this);
-			setContentView(webview);
 
-			webview.loadUrl("http://141.212.236.105:8888/vote");
-			webview.getSettings().setJavaScriptEnabled(true);
-			webview.setVerticalScrollBarEnabled(false);
-			webview.setHorizontalScrollBarEnabled(false);
-			webview.setWebViewClient(new VoteViewClient());
+					webview = new WebView(this);
+					setContentView(webview);
+
+					webview.loadUrl(HOST_ADDRESS);
+					webview.getSettings().setJavaScriptEnabled(true);
+					webview.setVerticalScrollBarEnabled(false);
+					webview.setHorizontalScrollBarEnabled(false);
+					VoteViewClient vvc = new VoteViewClient();
+					vvc.context = this;
+					webview.setWebViewClient(vvc);
 		}
 
 	@Override
